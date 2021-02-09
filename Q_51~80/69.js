4280bf005f327db3669359ab101b6ca4cf0fd7d7
Woody-Park <starface1009@gmail.com>
2021-02-10 00:30:54 +0900

    문제 69번 해답 누락 업로드

--- Q_51~80/69.js
+++ Q_51~80/69.js
@@ -66,19 +66,27 @@ function mulArray(front, back) {
             switch (i) {
                 case 0:
                     while (j < 3) {
-                        let r = [];
-                        r.push(flatA[i] * flatB[j]);
-                        var newA = r.reduce((acc, cur) => acc + cur);
+                        let t = [];
+                        //let count = 0;
+                        t.push(flatA[i] * flatB[j]);
+                        var newA = t.reduce((acc, cur) => acc + cur);
                         j++
+
                     }
-                    arr.push(newA);
+
+                    arr.push("A", newA);
                     console.log('결과', arr);
+
                     break;
                 case 1:
                     if (j === 1 || j === 3) {
-                        let newB = [flatA[i] * flatB[j]];
-                        console.log(newB);
+                        let r = [];
+                        r.push(flatA[i] * flatB[j]);
+                        var newB = r.reduce((acc, cur) => acc + cur);
+                        j++
                     }
+                    arr.push(newB);
+                    console.log('결과', arr);
 
                     break;
 
@@ -94,9 +102,13 @@ function mulArray(front, back) {
                     break;
                 case 3:
                     if (j === 1 || j === 3) {
-                        let newD = [flatA[i] * flatB[j]];
-                        console.log(newD);
+                        let u = [];
+                        u.push(flatA[i] * flatB[j]);
+                        var newD = u.reduce((acc, cur) => acc + cur);
+                        j++
                     }
+                    arr.push(newD);
+                    console.log('결과', arr);
 
                     break;
 
@@ -114,6 +126,7 @@ function mulArray(front, back) {
     console.log(`3---(${flatA[2] * flatB[0]}) + (${flatA[3] * flatB[1]})`);
     console.log(`4---(${flatA[2] * flatB[2]}) + (${flatA[3] * flatB[3]})`);
 
+    // 규칙..
     // a[0](1) = b[0,1,2];    [1 * 1, 1 * 0, 1 * 0];     ->   [1,0,0];  [1]
     // a[1](2) = b[1,3];      [2 * 0,  2 * 3];           ->   [0, 6];   [6]
     // a[2](2) = b[0,1,2];    [2 * 1,  2 * 0, 2 * 0];    ->   [2,0,0];  [2]
@@ -127,3 +140,44 @@ let b = [[1, 0], [0, 3]];
 
 mulArray(a, b);
 
+
+
+
+
+
+// 해답 
+
+// 문제를 푸는데 시간도 너무 오래 걸렸고, 다 풀지도 못했지만..
+// 1차원 배열로 만들어서 풀려고 했던게 오히려 더 어렵게 했던 것 같다.
+// for문에 if문에 while, switch 까지.. 
+// 복잡도도 커지고, 써야하는 코드 낭비도 너무 심할 것 같다는 생각이 들었다.
+// 규칙을 발견했지만, 배열이  3 * 3 이라면? 다양한 경우에서는 적용이 어려울 것이라는 생각이 들었다.
+// 배열 곱하기가 되지 않는 경우도 생각못했다.
+// 문제 푸는 시간을 제한 없이 풀어야겠다..
+
+function solution(a, b) {
+    let c = [];
+    const len = a[0].length;
+
+    if (len === b.length) {
+        for (let i = 0; i < len; i++) {
+            let row = [];
+            for (let j = 0; j < len; j++) {
+                let x = 0;
+                for (let k = 0; k < len; k++) {
+                    x += a[i][k] * b[k][j];
+                }
+                row.push(x);
+            }
+            c.push(row);
+        }
+        return c;
+    } else {
+        return -1;
+    }
+}
+
+const a = [[1, 2], [2, 4]];
+const b = [[1, 0], [0, 3]];
+
+console.log(solution(a, b));
